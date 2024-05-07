dbef679e12f1cf68b68d5060d25bd2c9b30b25ac
toa912 <toa912@naver.com>
2024-05-07 22:47:10 +0900

    KakoLogin (연결만)

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from './App'
-import Login from './DEOK/Login_SingUp/Login_Signup'
+import Login from './DEOK/Login_SingUp/Login_Signup';
+import KakaoLogin from './GUN/SocialKakao';
 
 function Routing() {
     return(
@@ -8,6 +9,7 @@ function Routing() {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/login" element={<Login />} />
+                <Route path="/login/Socialkakao" element={<KakaoLogin />} />
             </Routes>
         </BrowserRouter>
     )
