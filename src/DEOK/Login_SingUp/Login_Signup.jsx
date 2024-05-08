0a224d3fbad24b6df356f93efc8964b44a3e1edb
LeeEunDeok <t01092561402@gmail.com>
2024-05-08 12:35:00 +0900

    123

--- src/DEOK/Login_SingUp/Login_Signup.jsx
+++ src/DEOK/Login_SingUp/Login_Signup.jsx
@@ -1,98 +1,94 @@
 import React, { useState } from "react";
 import styles from "./Login_Signup.module.css";
-import { FaEye, FaEyeSlash, FaUser, FaEnvelope, FaLock } from "react-icons/fa";
-import { PiCalendarBold } from "react-icons/pi";
-import { GiSmartphone } from "react-icons/gi";
+import { FaEye, FaEyeSlash, FaEnvelope } from "react-icons/fa";
 import { IoHomeOutline } from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
-import kakao from "../../images/btn_kakao.svg";
-import google from "../../images/btn_google.svg";
-import naver from "../../images/btn_naver.svg";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 import { naverLoginUrl } from "../../SUNG/SocialNaver";
 
 const SignUpForm = () => {
-    const member_url = "http://localhost:8888/member/";
+    const member_url = "http://localhost:8988/member/";
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
+    const [name, setName] = useState("");
     const [nickname, setNickname] = useState("");
     const [phoneNumber, setPhoneNumber] = useState("");
     const [birthDate, setBirthDate] = useState("");
-    const [role, setRole] = useState("USER"); // 밑에서 체크하면 ADMIN 안하면 USER
+    const [role, setRole] = useState("USER");
 
     function validateInputs() {
         const errors = {};
 
-        // 이메일 유효성 검사
-        if (!email.trim()) {
-            errors.email = "이메일을 입력해주세요.";
-            alert(errors.email);
-        } else if (
-            !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(email)
-        ) {
-            errors.email =
-                "유효한 이메일을 입력해주세요.\n" +
-                "1. @를 포함해야합니다.\n" +
-                "2. .com과 같은 올바른 이메일 형식이어야 합니다.";
-            alert(errors.email);
-        } else {
-            // 비밀번호 유효성 검사
-            if (!password) {
-                errors.password = "비밀번호를 입력해주세요!!!!";
-                alert(errors.password);
-            } else if (password.length < 6) {
-                errors.password = "비밀번호는 최소 6자 이상이어야 합니다.";
-                alert(errors.password);
-            } else {
-                // 비밀번호 확인 유효성 검사
-                if (password !== confirmPassword) {
-                    errors.confirmPassword = "비밀번호가 일치하지 않습니다.";
-                    alert(errors.confirmPassword);
-                } else {
-                    // 닉네임 유효성 검사
-                    if (!nickname.trim()) {
-                        errors.nickname = "닉네임을 입력해주세요.";
-                        alert(errors.nickname);
-                    } else if (nickname.trim().length < 7) {
-                        errors.nickname = "닉네임은 7개 이상의 문자를 포함해야 합니다.";
-                        alert(errors.nickname);
-                    } else {
-                        // 전화번호 유효성 검사
-                        if (!phoneNumber.trim()) {
-                            errors.phoneNumber = "전화번호를 입력해주세요.";
-                            alert(errors.phoneNumber);
-                        } else if (!/^[0-9]{11}$/.test(phoneNumber)) {
-                            errors.phoneNumber = "유효한 전화번호 11자리를 입력해주세요. ";
-                            alert(errors.phoneNumber);
-                        } else {
-                            // 생년월일 유효성 검사
-                            if (!birthDate.trim()) {
-                                errors.birthDate = "생년월일을 입력해주세요.";
-                                alert(errors.birthDate);
-                            } else {
-                                // 생년월일 형식(YYYY-MM-DD) 검사
-                                const birthDateRegex = /^\d{4}\d{2}\d{2}$/;
-                                if (!birthDateRegex.test(birthDate)) {
-                                    errors.birthDate =
-                                        "유효한 생년월일을 입력해주세요. (YYYY-MM-DD)";
-                                    alert(errors.birthDate);
-                                } else {
-                                    // 생년월일 범위(1900년 이후) 검사
-                                    const year = parseInt(birthDate.substring(0, 4), 10);
-                                    if (year < 1900 || year > new Date().getFullYear()) {
-                                        errors.birthDate = "유효한 생년월일을 입력해주세요.";
-                                        alert(errors.birthDate);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        // // 이메일 유효성 검사
+        // if (!email.trim()) {
+        //     errors.email = "이메일을 입력해주세요.";
+        //     alert(errors.email);
+        // } else if (
+        //     !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i.test(email)
+        // ) {
+        //     errors.email =
+        //         "유효한 이메일을 입력해주세요.\n" +
+        //         "1. @를 포함해야합니다.\n" +
+        //         "2. .com과 같은 올바른 이메일 형식이어야 합니다.";
+        //     alert(errors.email);
+        // } else {
+        //     // 비밀번호 유효성 검사
+        //     if (!password) {
+        //         errors.password = "비밀번호를 입력해주세요!!!!";
+        //         alert(errors.password);
+        //     } else if (password.length < 6) {
+        //         errors.password = "비밀번호는 최소 6자 이상이어야 합니다.";
+        //         alert(errors.password);
+        //     } else {
+        //         // 비밀번호 확인 유효성 검사
+        //         if (password !== confirmPassword) {
+        //             errors.confirmPassword = "비밀번호가 일치하지 않습니다.";
+        //             alert(errors.confirmPassword);
+        //         } else {
+        //             // 닉네임 유효성 검사
+        //             if (!nickname.trim()) {
+        //                 errors.nickname = "닉네임을 입력해주세요.";
+        //                 alert(errors.nickname);
+        //             } else if (nickname.trim().length < 7) {
+        //                 errors.nickname = "닉네임은 7개 이상의 문자를 포함해야 합니다.";
+        //                 alert(errors.nickname);
+        //             } else {
+        //                 // 전화번호 유효성 검사
+        //                 if (!phoneNumber.trim()) {
+        //                     errors.phoneNumber = "전화번호를 입력해주세요.";
+        //                     alert(errors.phoneNumber);
+        //                 } else if (!/^[0-9]{11}$/.test(phoneNumber)) {
+        //                     errors.phoneNumber = "유효한 전화번호 11자리를 입력해주세요. ";
+        //                     alert(errors.phoneNumber);
+        //                 } else {
+        //                     // 생년월일 유효성 검사
+        //                     if (!birthDate.trim()) {
+        //                         errors.birthDate = "생년월일을 입력해주세요.";
+        //                         alert(errors.birthDate);
+        //                     } else {
+        //                         // 생년월일 형식(YYYY-MM-DD) 검사
+        //                         const birthDateRegex = /^\d{4}\d{2}\d{2}$/;
+        //                         if (!birthDateRegex.test(birthDate)) {
+        //                             errors.birthDate =
+        //                                 "유효한 생년월일을 입력해주세요. (YYYY-MM-DD)";
+        //                             alert(errors.birthDate);
+        //                         } else {
+        //                             // 생년월일 범위(1900년 이후) 검사
+        //                             const year = parseInt(birthDate.substring(0, 4), 10);
+        //                             if (year < 1900 || year > new Date().getFullYear()) {
+        //                                 errors.birthDate = "유효한 생년월일을 입력해주세요.";
+        //                                 alert(errors.birthDate);
+        //                             }
+        //                         }
+        //                     }
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
 
         return errors;
     }
@@ -112,10 +108,12 @@ const SignUpForm = () => {
                     // Spring 서버의 API 엔드포인트로 변경
                     email: email, // 이메일 필드에는 useState로 관리한 상태를 사용
                     password: password, // 비밀번호 필드에는 useState로 관리한 상태를 사용
+                    name: name,
                     nickname: nickname, // 닉네임 필드에는 useState로 관리한 상태를 사용
                     phone: phoneNumber, // 전화번호 필드에는 useState로 관리한 상태를 사용
                     birth: birthDate, // 생년월일 필드에는 useState로 관리한 상태를 사용
                     role: role, // 밑에서 체크하면 ADMIN 안하면 USER
+                    agreedPersonal: 1
                 })
                 .then((response) => {
                     // Handle success.
@@ -239,13 +237,13 @@ const SignUpForm = () => {
                         </div>
                         <div className={styles.btn_sns_login}>
                             <Link to={naverLoginUrl}>
-                                <img className={styles.naver} src={naver} alt="naver" />
+                                <img className={styles.naver} src='/images/btn_naver.svg' alt="naver" />
                             </Link>
                             <Link to="/login/Socialkakao">
-                                <img className={styles.kakao} src={kakao} alt="kakao" />
+                                <img className={styles.kakao} src='/images/btn_kakao.svg' alt="kakao" />
                             </Link>
                             <Link to="/">
-                                <img className={styles.google} src={google} alt="google" />
+                                <img className={styles.google} src='/images/btn_google.svg' alt="google" />
                             </Link>
                         </div>
                     </form>
@@ -266,7 +264,6 @@ const SignUpForm = () => {
                                 value={email}
                                 onChange={(event) => setEmail(event.target.value)}
                             />
-                            <FaEnvelope className={styles.icon} />
                         </div>
                         <div className={styles["input-box"]}>
                             <input
@@ -275,7 +272,6 @@ const SignUpForm = () => {
                                 value={password}
                                 onChange={(event) => setPassword(event.target.value)}
                             />
-                            <FaLock className={styles.icon} />
                         </div>
                         <div className={styles["input-box"]}>
                             <input
@@ -284,7 +280,14 @@ const SignUpForm = () => {
                                 value={confirmPassword}
                                 onChange={(event) => setConfirmPassword(event.target.value)}
                             />
-                            <FaLock className={styles.icon} />
+                        </div>
+                        <div className={styles["input-box"]}>
+                            <input
+                                type="text"
+                                placeholder="성함"
+                                value={name}
+                                onChange={(event) => setName(event.target.value)}
+                            />
                         </div>
                         <div className={styles["input-box"]}>
                             <input
@@ -293,7 +296,6 @@ const SignUpForm = () => {
                                 value={nickname}
                                 onChange={(event) => setNickname(event.target.value)}
                             />
-                            <FaUser className={styles.icon} />
                         </div>
                         <div className={styles["input-box"]}>
                             <input
@@ -302,7 +304,6 @@ const SignUpForm = () => {
                                 value={phoneNumber}
                                 onChange={(event) => setPhoneNumber(event.target.value)}
                             />
-                            <GiSmartphone className={`${styles.icon} ${styles.phone}`} />
                         </div>
                         <div className={styles["input-box"]}>
                             <input
@@ -311,7 +312,6 @@ const SignUpForm = () => {
                                 value={birthDate}
                                 onChange={(event) => setBirthDate(event.target.value)}
                             />
-                            <PiCalendarBold className={styles.icon} />
                         </div>
 
                         <div className={styles["remember-forgot"]}>
