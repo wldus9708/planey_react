07f4b3e23adfd2982dd6dec4043d04459cbab261
wldus9708 <kimwldus9833@naver.com>
2024-05-09 21:37:09 +0900

    카카오api 페이지 구현 60

--- src/YEON/planContent/planDay.jsx
+++ src/YEON/planContent/planDay.jsx
@@ -1,7 +1,7 @@
 function App(){
     return(
         <div>
-            임시test01
+            임시test02
         </div>
     );    
 }
--- src/YEON/planSidebar.jsx
+++ src/YEON/planSidebar.jsx
@@ -4,10 +4,10 @@ import styles from "./planSidebar.module.css";
 
 const Sidebar = ({ onButtonClick }) => {
     return (
-        <div className={styles.sidebar}> {/* 'sidebar' 클래스 추가 */}
-            <button onClick={() => onButtonClick('component1')}>메인</button>
-            <button onClick={() => onButtonClick('component2')}>날짜 확인</button>
-            <button onClick={() => onButtonClick('component3')}>명소 및 숙소 확인</button>
+        <div className={styles.plansidebar}> {/* 'sidebar' 클래스 추가 */}
+            <button className={styles.planbutton} onClick={() => onButtonClick('component1')}>메인</button>
+            <button className={styles.planbutton} onClick={() => onButtonClick('component2')}>날짜 확인</button>
+            <button className={styles.planbutton} onClick={() => onButtonClick('component3')}>명소 및 숙소 확인</button>
         </div>
     );
 };
