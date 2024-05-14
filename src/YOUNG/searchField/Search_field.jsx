12e20d3912fabefac3fe2ad479bff2198ffa7129
young <young@naver.com>
2024-05-14 21:29:13 +0900

    searchfield 지역선택, 날짜 선택 (+)인원선택(-)

--- src/YOUNG/searchField/Search_field.jsx
+++ src/YOUNG/searchField/Search_field.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useRef } from "react";
+import { Tabs, Tab } from 'react-bootstrap'; // React Bootstrap의 탭 컴포넌트 사용
 import styles from "./Search_field.module.css";
 import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
+import 'react-calendar/dist/Calendar.css';
 import { format } from 'date-fns';
+import ko from 'date-fns/locale/ko'; // 'ko'를 가져옴
+
 
 function SearchField() {
-   // 달력 
+
    const [date, setDate] = useState([
       {
          startDate: new Date(),
@@ -13,98 +17,162 @@ function SearchField() {
          key: 'selection'
       }
    ]);
-
    const [openDate, setOpenDate] = useState(false);
-   const calendarRef = useRef(null); // 캘린더 참조
+   const calendarRef = useRef(null);
 
    const handleCalendarClick = (e) => {
-      // 캘린더 영역을 클릭했을 때는 캘린더를 닫지 않음
       if (calendarRef.current && calendarRef.current.contains(e.target)) {
          return;
       }
       setOpenDate(false);
+
    };
 
    const handleConfirm = () => {
-      setOpenDate(false);   
-      // 선택한 날짜를 저장하거나 다른 작업 수행
+      setOpenDate(false);
       console.log("Selected date range:", date);
    };
 
-//   // 인원수 조절 
-//   const [adults, setAdults] = useState(1);
-//   const [children, setChildren] = useState(0);
-//   const [openModal, setOpenModal] = useState(false);
 
-//   const handleOpenModal = () => {
-//      setOpenModal(true);
-//   };
 
-//   const handleCloseModal = () => {
-//      setOpenModal(false);
-//   };
+   //  ===========================================
+   // 검색어로 지역 찾기
 
-//   const handleDecrement = (type) => {
-//      if (type === 'adults' && adults > 0) {
-//         setAdults(adults - 1);
-//      } else if (type === 'children' && children > 0) {
-//         setChildren(children - 1);
-//      }
-//   };
+   // 검색어 목록 (가정)
+   const suggestions = ['강원', '제주', '강남', '송파구', '부산', '부천', '동대문', '인천', '홍대', '홍콩'];
 
-//   const handleIncrement = (type) => {
-//      if (type === 'adults') {
-//         setAdults(adults + 1);
-//      } else if (type === 'children') {
-//         setChildren(children + 1);
-//      }
-//   };
+   // 검색어와 입력값을 관리하는 상태
+   const [input, setInput] = useState('');
+   const [matchingSuggestions, setMatchingSuggestions] = useState([]);
+
+   // 입력값이 변경될 때마다 관련 검색어를 찾아 상태를 업데이트하는 함수
+   const handleInputChange = (e) => {
+      const inputValue = e.target.value.toLowerCase();
+      setInput(inputValue);
+      if (inputValue) {
+         const matching = suggestions.filter(suggestion =>
+            suggestion.toLowerCase().includes(inputValue)
+         );
+         setMatchingSuggestions(matching);
+      } else {
+         setMatchingSuggestions([]); // 입력값이 없을 때는 제안을 숨김
+      }
+   };
+
+   // 검색어를 클릭하면 입력값을 해당 검색어로 설정하고 관련 검색어를 비움
+   const handleSuggestionClick = (suggestion) => {
+      setInput(suggestion);
+      setMatchingSuggestions([]);
+   };
+
+
+
 
 
    return (
       <section className={styles.home}>
-         <div className={`${styles.secContainer} container`} onClick={handleCalendarClick}>
-            <div className={`${styles.homeCard} grid`}>
-               <div className={styles.locationDiv}>
-                  <label htmlFor="locationInput"></label>
-                  <input
-                     id="locationInput"
-                     type="text"
-                     placeholder="지역 입력"
-                  />
-               </div>
 
-               <div className={`${styles.calendar} ${openDate ? styles.calendarOpen : ''}`} ref={calendarRef}>
-                  <span onClick={() => setOpenDate(!openDate)}>
-                     {`${format(date[0].startDate, "yyyy.MM.dd")} ~ ${format(date[0].endDate, "yyyy.MM.dd")}`} {/* 시작 날짜와 종료 날짜 표시 */}
-                  </span>
-         
-                  {openDate && <DateRange
-                     editableDateInputs={true}
-                     onChange={(item) => setDate([item.selection])}
-                     moveRangeOnFirstSelection={false}
-                     ranges={date}
-                     className={styles.date}
-                  />}
-                  {openDate && (
-                     <div>
-                        <button onClick={handleConfirm}>확인</button>
+         <div
+            className={`${styles.secContainer} container`}
+            onClick={handleCalendarClick}>
+
+
+            <Tabs
+               defaultActiveKey="region"
+               id="uncontrolled-tab-example"
+               className="mb-3"
+               fill>
+
+               <Tab
+                  eventKey="region"
+                  title="지역선택"
+                  className={styles.regionPicker} >
+                  <div>
+                     <input
+                        type="text"
+                        placeholder="&nbsp;&nbsp;&nbsp;&nbsp;지역을 검색 해보세요."
+                        value={input}
+                        onChange={handleInputChange}
+                        className={styles.regionPicker}
+                     />
+                     <div className={styles.searchList}>
+                        <ul>
+                           {matchingSuggestions.map((suggestion, index) => (
+                              <li
+                                 key={index}
+                                 onClick={() => handleSuggestionClick(suggestion)}
+                              >
+                                 {suggestion}
+                              </li>
+                           ))}
+                        </ul>
                      </div>
-                  )}
+                  </div>
+
+               </Tab>
+               {/* 날짜 고르기 */}
+               <Tab
+                  eventKey="date"
+                  title="날짜 선택"
+                  className={styles.calendarPicker}>
+                  <div className={`${styles.calendar} ${openDate ? styles.calendarOpen : ''}`} ref={calendarRef}>
+                     <span
+                        className={styles.dateCheck}
+                        onClick={() => setOpenDate(!openDate)}>
+                        {`${format(date[0].startDate, "yyyy.MM.dd(eee)")} - ${format(date[0].endDate, "yyyy.MM.dd(eee)")}`}
+                     </span>
+
+                     {openDate && <DateRange
+                        editableDateInputs={false}
+                        onChange={(item) => setDate([item.selection])}
+                        moveRangeOnFirstSelection={false}
+                        ranges={date}
+                        className={styles.date}
+                        locale={ko}
+                     />}
+                     {openDate && (
+                        <div>
+                           <button
+                              className={styles.checkBtn}
+                              onClick={handleConfirm}>확인</button>
+                        </div>
+                     )}
 
 
-                  <button data-aos="zoom-in-up" className={styles.btn}>
-                     Search
-                  </button>
-               </div>
-              
 
-            </div>
+                  </div>
 
-
+                  {/* <div
+                     className={styles.CalendarContainer}>
+                     <button
+                        onClick={handleToggleCalendar}
+                        className={styles.DropdownButton}
+                     >{nowDate}</button>
+                     <div
+                        className= {styles.CalendarWrapper}
+                        style={{ display: isOpen ? 'block' : 'none' }}>
+                        <Calendar onChange={handleDateChange}
+                           value={value}
+                           formatDay={(locale, date) => moment(date).format("DD")}></Calendar>
+                     </div>
+                  </div> */}
+               </Tab>
+               <Tab
+                  eventKey="count"
+                  title="인원 선택">
+               </Tab>
+               <button data-aos="zoom-in-up" className={styles.btn}>
+                  Search
+               </button>
+            </Tabs>
          </div>
+
       </section>
    );
 }
 
 export default SearchField;
+
+
+
+
