7ca0f2bda6ac0484341bd4b213e73f2ebd636391
awd9074 <awd9074@gamil.com>
2024-05-16 21:31:31 +0900

    광고 커밋

--- src/Hye/advertisement.jsx
+++ src/Hye/advertisement.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, Component } from 'react';
 import axios from 'axios';
-import './TestComponent.scss'; // 스타일링 파일
-import img1 from './component/바다01.jpg'
+import './advertisementComponent.scss'; // 스타일링 파일
 const TestComponent = () => {
     const [dataset, setDataSet] = useState(null);
     const [randomAd, setRandomAd] = useState(null); // 랜덤으로 선택된 광고를 저장할 상태
-
+    const [sendData, setSendData]=useState(null);
+    const [updataset, setUpdataSet] = useState(null);
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData1 = async () => {
             try {
                 const response = await axios.get('http://localhost:8988/admin/advertises/all');
                 console.log(response.data); // 성공 시 응답 데이터 출력
@@ -19,12 +19,14 @@ const TestComponent = () => {
                     setRandomAd(activeAds[randomIndex]); // 랜덤으로 하나 선택
                 }
             } catch (error) {
-                console.error('Error fetching data:', error);
-                setDataSet({ error: 'Error fetching data' }); // 오류 메시지를 저장
+                console.error('Error fetching data1:', error);
+                setDataSet({ error: 'Error fetching data1' }); // 오류 메시지를 저장
             }
         };
-
-        fetchData(); // 데이터 가져오기
+       
+               
+        // fetchData 함수 호출
+        fetchData1(); // 데이터 가져오기
     }, []); // 빈 배열을 두 번째 인자로 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 설정합니다.
 
     return (
@@ -32,12 +34,12 @@ const TestComponent = () => {
             {randomAd ? (
                 <div className="advertisement-item">
                     <div className="advertisement-image">
-                <img src={require(`./component/${randomAd.image}`)} alt={randomAd.title} />
+                <img className="advertisement-img" src={require(`./component/${randomAd.image}`)} alt={randomAd.title} />
                 </div>
                 <div className="advertisement-content">
-                    <p>{randomAd.title}</p>
-                    <p>{randomAd.content}</p>
-                    <p>{randomAd.image}</p>
+                    <h1>{randomAd.title}</h1>
+                    <h3>{randomAd.content}</h3>
+                    <h3>{randomAd.image}</h3>
                 </div>
                 
             </div>
