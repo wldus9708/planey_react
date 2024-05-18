185e66442a17a6bfd94c21506856f466f497db3b
young <young@naver.com>
2024-05-18 18:34:19 +0900

    숙소디테일 스프링연결 (+)

--- src/YOUNG/lodging/LodgingDetail.jsx
+++ src/YOUNG/lodging/LodgingDetail.jsx
@@ -3,29 +3,31 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import styles from './LodgingDetail.module.css';
 import axios from "axios";
 import LodgingPayment from "./LodingPayment";
+import { useParams } from "react-router-dom";
 
 
 const LodgingDetail = () => {
+  let { id } = useParams(); // URL에서 숙소 ID 가져오기
+  const [lodging, setLodging] = useState(null);
   // 이미지 목록을 위한 상태 추가
-  const [imageList, setImageList] = useState([]);
   const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const [numberOfPeople, setNumberOfPeople] = useState(1); // 인원 수
 
+  // 스프링 연결 
   useEffect(() => {
-    axios.get("http://localhost:8988/api/images/lodImage")
-      .then((response) => {
-        setImageList(response.data) // 이미지 목록 상태 업데이트
-        setActiveImageIndex(0) // 초기 인덱스 설정 
-      })
-      .catch(error => {
-        console.error('Error fetching image names:', error)
-      });
+    axios.get(`http://localhost:8988/lodging/detail/${id}`)
+    .then((response) => {
+      if (response.data) {
+        setLodging(response.data); // 숙소 정보 업데이트
+        console.log(response.data);
+      }
+      setActiveImageIndex(1); // 초기 인덱스 설정
+    })
+    .catch(error => {
+      console.error('이미지 이름을 가져오는 중 오류 발생:', error);
+    });
 
-  }, []); // 빈 종속성 배열로, 한 번만 실행
-
-  // 이미지 목록이 업데이트되었는지 확인 후 렌더링
-  if (imageList.length === 0) {
-    return <div>Loading...</div>;
-  }
+  }, [id]);
 
   return (
 
@@ -36,24 +38,21 @@ const LodgingDetail = () => {
             <div className={styles.lodgingDivLeft}>
               <div className={styles.imgContainer}>
             
-                <img
-                  src={`/images/${imageList[activeImageIndex]}`}
-                  alt={`aaa0${activeImageIndex + 1}`}
+              <img
+                  src={`/images/${lodging && lodging[`lodImage0${activeImageIndex + 1}`]}`}
+                  alt={`images${activeImageIndex + 1}`}
                 />
               </div>
               <div className={styles.hoverContainer}>
-                {/* 각 이미지에 마우스 오버 이벤트 추가 */}
-                {imageList.map((image, index) => (
-
+                {[1, 2, 3, 4, 5].map(index => (
                   <div
                     key={index}
-                    className={
-                      index === activeImageIndex ? styles.active : ''
-                    } // 'active' 클래스 적용
-                    onMouseOver={() => setActiveImageIndex(index)} // 이벤트 처리
+                    className={styles.lodDetailactive}
+                    onMouseOver={() => setActiveImageIndex(index - 1)}
                   >
                     <img
-                      src={`/images/${image}`}
+                      src={`/images/${lodging && lodging[`lodImage0${index}`]}`}
+                      alt={`images${index}`}
                     />
                   </div>
                 ))}
@@ -64,22 +63,22 @@ const LodgingDetail = () => {
                 <img src="/images/star.png" alt="star"/> &nbsp;&nbsp;&nbsp;5.0
                 </div>
               <div className={styles.lodgingHeader}>
-                <span className={styles.lodgingName}>숲속 공주 하우스</span>
+                <span className={styles.lodgingName}>{lodging && lodging.lodName}</span>
                 
-                <span className={styles.lodgingType}>펜션</span>
+                <span className={styles.lodgingType}>{lodging && lodging.lodCategory}</span>
                 <br />
             
               </div>
 
               <hr />
               <br />
-              <span className={styles.lodgingAddress}>주소: 서울시 은평구 역촌촌촌</span>
+              <span className={styles.lodgingAddress}>주소: {lodging && lodging.lodAddress}</span>
+              <span className={styles.lodgingAddress}>상세 주소: {lodging && lodging.lodAddressDetail}</span>
               <br />
-              <span className={styles.lodgingPrice}>150,000,000원</span>
+              <span className={styles.lodgingPrice}>{lodging && lodging.lodPrice.toLocaleString()}원</span>
 
               <p className={styles.lodgingDescription}>
-                잠자는 숲 속의 공주가 자고 있어요 어서 와서 깨워 주세요. 쿨쿨쿨쿨 .... 음냐아아
-                놀러가고싶다아아아아아 예쁜 숙소 군요 바베큐 파뤼 하고 싶어요 ~~
+              {lodging && lodging.lodDescription}
               </p>
                 <div>
               <LodgingPayment/>
