4cc50730d197b2ff9425555c1eb1cb46a28692de
toa912 <toa912@naver.com>
2024-05-18 21:54:57 +0900

    add RentCar Detail

---
+++ src/GUN/RentCarIndex.jsx
@@ -0,0 +1,16 @@
+import RentCarHead from "./components/RentCarHead";
+import RentCarBody from "./components/RentCarBody";
+import RentCarFoot from "./components/RentCarFoot";
+
+
+
+function App(){
+    return(
+        <div>
+            <RentCarHead></RentCarHead>
+            <RentCarBody></RentCarBody>
+            <RentCarFoot></RentCarFoot>
+        </div>
+    );    
+}
+export default App ;
