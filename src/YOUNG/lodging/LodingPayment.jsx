86c376da8bbc42c0ef1de74c016446cefeeae821
BBS <bbs4861@naver.com>
2024-05-23 18:46:14 +0900

    숙소, 숙소페이먼츠 커밋

--- src/YOUNG/lodging/LodingPayment.jsx
+++ src/YOUNG/lodging/LodingPayment.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from './LodingPayment.module.css';
 import stylesBtn from "./LodgingDetail.module.css"
 
-const LodingPayment = () => {
+const LodingPayment = ({ reservations }) => {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate] = useState(new Date());
     const [adults, setAdults] = useState(1);
@@ -18,7 +18,24 @@ const LodingPayment = () => {
         setChildren(prev => Math.max(0, prev + increment));
     };
 
-  
+    // 예약된 날짜 범위 계산
+    const getHighlightedDates = () => {
+        const highlightedDates = [];
+        reservations.forEach(reservation => {
+            const start = new Date(reservation.lodDepartureDate);
+            const end = new Date(reservation.lodArrivalDate);
+            for (let d = start; d <= end; d.setDate(d.getDate() + 1)) {
+                highlightedDates.push(new Date(d));
+            }
+        });
+        return highlightedDates;
+    };
+
+    const highlightDates = [
+        {
+            "react-datepicker__day--highlighted-custom-1": getHighlightedDates()
+        }
+    ];
 
     return (
         <div className={styles.modal}>
@@ -31,6 +48,7 @@ const LodingPayment = () => {
                     placeholderText="날짜를 선택하세요"
                     className={styles.dateInput}
                     minDate={new Date()} // 시작 날짜 이후로만 선택 가능
+                    highlightDates={highlightDates}
                 />
             </div>
             <div className={styles.datePicker}>
@@ -42,6 +60,7 @@ const LodingPayment = () => {
                     placeholderText="날짜를 선택하세요"
                     className={styles.dateInput}
                     minDate={startDate} // 시작 날짜 이후로만 선택 가능
+                    highlightDates={highlightDates}
                 />
             </div>
             <div className={styles.counter}>
@@ -55,7 +74,7 @@ const LodingPayment = () => {
                 <span>{children}</span>
                 <button onClick={() => handleChildrenChange(1)}>+</button>
             </div>
-              <div className={stylesBtn.btnGroups}>
+            <div className={stylesBtn.btnGroups}>
                 <button type="button" className={stylesBtn.addCartBtn}>
                   <i className='fas fa-shopping-cart'></i>
                   장바구니 추가
@@ -64,8 +83,7 @@ const LodingPayment = () => {
                   <i className='fas fa-wallet'></i>
                   예약 하러 가기
                 </button>
-
-              </div>
+            </div>
         </div>
     );
 };
