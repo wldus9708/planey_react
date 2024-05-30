57b31058e7af7ae6f5b75eee4e860860a4c8ffd7
awd9074 <awd9074@gamil.com>
2024-05-30 11:37:15 +0900

    패키지 투어 디버깅

--- src/GUN/PackageIndex.jsx
+++ src/GUN/PackageIndex.jsx
@@ -1,12 +1,13 @@
 import PackageHead from "./components/PackageHead";
 import PackageBody from "./components/PackageBody";
 import PackageFoot from "./components/PackageFoot";
-
+import Navbar from "../CKH/Components/Navbar/Navbar";
 
 
 function App(){
     return(
         <div>
+            <Navbar />
             <PackageHead></PackageHead>
             <PackageBody></PackageBody>
             <PackageFoot></PackageFoot>
